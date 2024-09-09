37cbcb41b1dc6d759db7d0e337b661fad62109bd
Danil <dabilsch60@gmail.com>
2024-09-09 23:15:17 +0300

    try 2

--- src/main/java/org/example/print_project_prices.sql
+++ src/main/java/org/example/print_project_prices.sql
@@ -1,6 +1,10 @@
-SELECT  project.ID, TIMESTAMPDIFF(month, START_DATE, FINISH_DATE) * SUM(SALARY)
-      from project, worker
-      group by id;
+SELECT project.ID as project,
+       TIMESTAMPDIFF(month, project.START_DATE, project.FINISH_DATE) * SUM(worker.SALARY) AS project_cost
+FROM project
+         JOIN project_worker ON project.ID = project_worker.PROJECT_ID
+         JOIN worker ON project_worker.WORKER_ID = worker.ID
+GROUP BY project.ID
+ORDER BY project_cost DESC;
 
 
 
